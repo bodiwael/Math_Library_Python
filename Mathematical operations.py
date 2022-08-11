501ecd3734d4acfc93da9c09d201fc99934de8f3
Abdelrahman Wael Abdelrahman Rashad Ammar <73370043+bodiwael@users.noreply.github.com>
2022-08-11 23:11:00 +0200

    Add files via upload

---
+++ Mathematical operations.py
@@ -0,0 +1,81 @@
+import math as m                #               math library
+
+a  =  m.factorial(10)            #               Factorial
+
+print(a)
+
+a = m.exp(7)                     #               Exponential
+
+print(a)
+
+a = m.log(7)                     #               Log depend on one base
+
+print(a)
+
+a = m.log(16,2)                  #              Depends on two numbers
+
+print(a)
+
+a = m.log10(100)
+
+print(a)
+
+a = m.sqrt(9)                    #                   Square Root
+
+print(a)
+
+a = m.degrees(360)               #   Converting from Radians to degress                       
+
+print(a)
+
+a = m.sin(m.radians(30))
+
+print(a)
+
+a = m.asin(m.radians(30))
+
+print(a)
+
+a = m.sinh(60)
+
+print(a)
+
+a = m.asinh(60)
+
+print(a)
+
+a = m.pi
+
+print(a)
+
+a = m.e
+
+print(a)
+
+a = m.tau
+
+print(a)
+
+a = m.inf
+
+print(a)
+
+a = m.copysign(7, -3)
+
+print(a)
+
+a = m.ceil(5.1)
+
+print(a)
+
+a = m.floor(5.9999)
+
+print(a)
+
+a = m.erf(8)
+
+print(a)
+
+a = m.gamma(8)
+
+print(a)
